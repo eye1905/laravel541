620c764debe0c2725eb1a61df71fe7043ce73070
eye1905 <cfcyoga@gmail.com>
2019-11-28 22:18:30 +0700

    udpate lago

--- resources/views/admin/detailProses/index.blade.php
+++ resources/views/admin/detailProses/index.blade.php
@@ -73,7 +73,7 @@
                @foreach ($masterdetailprosess as $key => $m)
                <tr>
                 <td>{{ $key+1 }}</td>
-                <td style="@if($m->parent==2) padding-left:4%; @endif" >{{ $masterbarangs[$m->id_barang]["namaBarang"] }}</td>
+                <td style="" >{{ $masterbarangs[$m->id_barang]["namaBarang"] }}</td>
                 <td>{{ $m->jumlahBarang }}</td>
                 <td>{{ $m->jumlah }}</td>
                 @if($m->status==1 or $m->status==7)
