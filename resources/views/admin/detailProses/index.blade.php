ef95c49ff64a3bf8294769e425e8bdb9926ac5c5
eye1905 <cfcyoga@gmail.com>
2019-12-08 22:39:36 +0700

    updat lagi

--- resources/views/admin/detailProses/index.blade.php
+++ resources/views/admin/detailProses/index.blade.php
@@ -82,7 +82,7 @@
                 <td>{{ $m->parent }}</td> --}}
                 <td>{{ $m->jumlah }}</td>
                 <td>{{ $status[$m->status] }}</td>
-                <td>{{ date("d-m-Y", strtotime($m->created_at)) }}</td>
+                <td>{{ date("d-m-Y H:i:s", strtotime($m->created_at)) }}</td>
                 <td>
                   @if($data->status==0)
                       @if($masterbarangs[$m->id_barang]["namaBarang"]=="Raw" and $m->status==0)
@@ -118,7 +118,7 @@
                 <td>{{ $value1->parent }}</td> --}}
                 <td>{{ $value1->jumlah }}</td>
                 <td>{{ $status[$value1->status] }}</td>
-                <td>{{ date("d-m-Y", strtotime($value1->created_at)) }}</td>
+                <td>{{ date("d-m-Y H:i:s", strtotime($value1->created_at)) }}</td>
                 <td>
                   @if($data->status==0)
                       @if($value1->status==1)
@@ -141,7 +141,7 @@
                 <td>{{ $value2->parent }}</td> --}}
                 <td>{{ $value2->jumlah }}</td>
                 <td>{{ $status[$value2->status] }}</td>
-                <td>{{ date("d-m-Y", strtotime($value2->created_at)) }}</td>
+                <td>{{ date("d-m-Y H:i:s", strtotime($value2->created_at)) }}</td>
                 <td>
                   @if($data->status==0)
                       @if($value2->status==2)
@@ -170,7 +170,7 @@
                   <td>{{ $value3->parent }}</td> --}}
                   <td>{{ $value3->jumlah }}</td>
                   <td>{{ $status[$value3->status] }}</td>
-                  <td>{{ date("d-m-Y", strtotime($value3->created_at)) }}</td>
+                  <td>{{ date("d-m-Y H:i:s", strtotime($value3->created_at)) }}</td>
                   <td>
                     @if($data->status==0)
                       @if($value3->status==3)
@@ -198,7 +198,7 @@
                     <td>{{ $value4->parent }}</td> --}}
                     <td>{{ $value4->jumlah }}</td>
                     <td>{{ $status[$value4->status] }}</td>
-                    <td>{{ date("d-m-Y", strtotime($value4->created_at)) }}</td>
+                    <td>{{ date("d-m-Y H:i:s", strtotime($value4->created_at)) }}</td>
                     <td>
                       @if($data->status==0)
                         @if($value4->status==3)
